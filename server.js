2e339867b4be0ca2e3ddd999cb2ae6c2193b0836
Habibullo Hamidov <h.hamidov77777@gmail.com>
2024-05-17 20:58:31 +0900

    feat: EJS Frameworkda Portfolio publishing qilamiz

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ console.log("Web Serverni boshlash");
 const express = require("express");
 const app = express();
 const http = require('http');
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+	if (err) {
+		console.log("ERROR:", err);
+	} else {
+		user = JSON.parse(data);
+	}
+});
 
 //1: Kirish code: expressga kirib kelayotgan malumotlarga bog'liq kod yoziladi
 app.use(express.static("public")); // har qand browserdan kirib kelgan zaproslar uchun public folder ochiq degan manoni bildiradi
@@ -19,6 +29,10 @@ app.set('view engine', 'ejs');
 app.post("/create-item", (req, res) => {
     console.log(req.body);
     res.json({test: "succes"});
+});
+
+app.get("/author", (req, res) => {
+	res.render("author", { user: user });
 });
 
 app.get("/", function(req, res) {
