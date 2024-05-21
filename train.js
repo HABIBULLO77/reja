0224472f03baedf1cb9e5d7b55ac20f0fc7cba86
Habibullo Hamidov <h.hamidov77777@gmail.com>
2024-05-21 23:57:51 +0900

    feat: async function and MIT A-Task Solution

--- train.js
+++ train.js
@@ -13,28 +13,71 @@ const list = [
   "endi dam oling, foydasi yo'q endi", // 60 
 ];
 
+// CALLBACK FUNCTION
+// function maslahatBering(a, callback) {
+//     if(typeof a !== 'number') callback("insert a number", null);
+//     else if(a <= 20) callback(null, list[0]);
+//     else if(a > 20 && a <= 30) callback(null, list[1]);
+//     else if(a > 30 && a <= 40) callback(null, list[2]);
+//     else if(a > 40 && a <= 50) callback(null, list[3]);
+//     else if(a > 50 && a <= 60) callback(null, list[4]);
+//     else {
+//         setInterval(function() {
+//             callback(null, list[5]);
+//         }, 1000);
+//     }
+// }
+// console.log('passed here 0');
+// maslahatBering(65, (err, data) => {
+//     if(err) console.log('ERROR:', err);
+//     console.log('javob:', data);
+// });
+// console.log('passed here 1');
 
-function maslahatBering(a, callback) {
-    if(typeof a !== 'number') callback("insert a number", null);
-    else if(a <= 20) callback(null, list[0]);
-    else if(a > 20 && a <= 30) callback(null, list[1]);
-    else if(a > 30 && a <= 40) callback(null, list[2]);
-    else if(a > 40 && a <= 50) callback(null, list[3]);
-    else if(a > 50 && a <= 60) callback(null, list[4]);
-    else {
-        setTimeout(function() {
-            callback(null, list[5]);
-        }, 5000);
-    }
-}
-console.log('passed here 0');
-maslahatBering(65, (err, data) => {
-    if(err) console.log('ERROR:', err);
-    console.log('javob:', data);
-});
-console.log('passed here 1');
+// ASYNC FUNCTION
+// async function maslahatBering(a) {
+//     if(typeof a !== 'number') throw new Error("insert a number");
+//     else if(a <= 20) return list[0];
+//     else if(a > 20 && a <= 30) return list[1];
+//     else if(a > 30 && a <= 40) return list[2];
+//     else if(a > 40 && a <= 50) return list[3];
+//     else if(a > 50 && a <= 60) return list[4];
+//     else {
+//         return new Promise((resolve, reject)=> {
+//             setTimeout(()=> {
+//                 resolve(list[5]);
+//             }, 5000)
+//         })
+//         //return list[5];
+//         // setTimeout(function() {
+//         //     return list[5];
+//         // }, 5000);
+//     }
+// }
+// console.log('passed here 0');
+// maslahatBering(65, (err, data) => {
+//     if(err) console.log('ERROR:', err);
+//     console.log('javob:', data);
+// });
 
+// call via then/catch
+// maslahatBering(-25).then(data => {
+//     console.log('javob:', data);
+// }).catch(err => {
+//     console.log('ERROR:', err);
+// })
+// console.log('passed here 1');
 
+// call via async/await
+// async function run() {
+//     let javob = await maslahatBering(65);
+//     console.log(javob);
+//     javob = await maslahatBering(31);
+//     console.log(javob);
+//     javob = await maslahatBering(41);
+//     console.log(javob);
+// }
+// run();
 
 
 /* A-TASK: 
