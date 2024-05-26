ca419f06b0a861c36313f8c4e6a2dd45f149f843
Habibullo Hamidov <h.hamidov77777@gmail.com>
2024-05-26 11:39:58 +0900

    feat: MITTask-C

--- train.js
+++ train.js
@@ -1,22 +1,85 @@
 console.log("TRAIN AREA");
 
+/* MITASK-C 
+
+Shunday class tuzing tuzing nomi Shop, va uni constructoriga 3 hil mahsulot pass bolsin,
+hamda classning 3ta methodi bolsin, biri qoldiq, biri sotish va biri qabul. 
+Har bir method ishga tushgan vaqt ham log qilinsin.
+MASALAN: const shop = new Shop(4, 5, 2); 
+shop.qoldiq() return hozir 20:40da 4ta non, 
+5ta lagmon va 2ta cola mavjud! 
+shop.sotish('non', 3) & shop.qabul('cola', 4) & shop.qoldiq()
+return hozir 20:50da 1ta non, 5ta lagmon va 6ta cola mavjud! 
+*/
+
+class Shop {
+    constructor(non, lagmon, cola) {
+        this.products = {
+            non: non,
+            lagmon: lagmon,
+            cola: cola
+        };
+    }
+
+    getCurrentTime() {
+        const now = new Date();
+        return now.getHours() + ":" + now.getMinutes();
+    }
+
+    qoldiq() {
+        const time = this.getCurrentTime();
+        console.log(`Hozir ${time}da ${this.products.non}ta non, ${this.products.lagmon}ta lagmon va ${this.products.cola}ta cola mavjud!`);
+        return `Hozir ${time}da ${this.products.non}ta non, ${this.products.lagmon}ta lagmon va ${this.products.cola}ta cola mavjud!`;
+    }
+
+    sotish(mahsulot, miqdor) {
+        if (this.products[mahsulot] !== undefined && this.products[mahsulot] >= miqdor) {
+            this.products[mahsulot] -= miqdor;
+            const time = this.getCurrentTime();
+            console.log(`Hozir ${time}da ${miqdor}ta ${mahsulot} sotildi.`);
+        } else {
+            console.log(`Mavjud emas yoki yetarli miqdorda emas.`);
+        }
+    }
+
+    qabul(mahsulot, miqdor) {
+        if (this.products[mahsulot] !== undefined) {
+            this.products[mahsulot] += miqdor;
+            const time = this.getCurrentTime();
+            console.log(`Hozir ${time}da ${miqdor}ta ${mahsulot} qabul qilindi.`);
+        } else {
+            console.log(`Noto'g'ri mahsulot nomi.`);
+        }
+    }
+}
+
+// Funksiyani sinab ko'rish
+const shop = new Shop(4, 5, 2);
+shop.qoldiq(); 
+shop.sotish('non', 3); 
+shop.qabul('cola', 4); 
+shop.qoldiq();
+
+
+
+
 /*B-TASK: 
 
 Shunday function tuzing, u 1ta string parametrga ega bolsin, hamda osha stringda qatnashgan raqamlarni sonini bizga return qilsin.
 MASALAN countDigits("ad2a54y79wet0sfgb9") 7ni return qiladi.
 */
-function countDigits(str) {
-    let count = 0
-    for (let i = 0; i < str.length; i++) {
-        if (str[i] >= '0' && str[i] <= '9') {
-            count++
-        }
-    }
-    return count;
-}
+// function countDigits(str) {
+//     let count = 0
+//     for (let i = 0; i < str.length; i++) {
+//         if (str[i] >= '0' && str[i] <= '9') {
+//             count++
+//         }
+//     }
+//     return count;
+// }
 
-const result = countDigits("ad2a54y79wet0sfgb9");
-console.log("Result: ", result);
+// const result = countDigits("ad2a54y79wet0sfgb9");
+// console.log("Result: ", result);
 // NodeJs event loop va Callback functionlarni o'rganamiz
 
 // console.log("Jack Ma Maslahaltlari!");
