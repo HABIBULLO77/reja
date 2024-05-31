8077411f2e425c7e754a20446529acd387c785e8
Habibullo Hamidov <h.hamidov77777@gmail.com>
2024-05-31 10:36:39 +0900

    fix: PORT changed to 3047

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ mongodb.connect(connectionString, {
         module.exports = client;
         const app = require("./app");
         const server = http.createServer(app);
-        let PORT = 3000;
+        //let PORT = 3000;
+        let PORT = 3047;
         server.listen(PORT, function(){
         console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`);
 });
