3bb4d4b9e059ae2d46fde7db07145c572a093ae5
Habibullo Hamidov <h.hamidov77777@gmail.com>
2024-06-03 01:37:20 +0900

    feat: MITASK-G Done

--- train.js
+++ train.js
@@ -1,42 +1,42 @@
 console.log("TRAIN AREA");
 
-/*F-TASK: 
+/*
+  G-TASK: 
 
-Shunday findDoublers function tuzing, unga faqat bitta string argument pass bolib, agar stringda bir hil harf qatnashgan bolsa true, qatnashmasa false qaytarishi kerak.
-MASALAN: findDoublers("hello") return true return qiladi
+Shunday function tuzingki unga integerlardan iborat array pass bolsin va function bizga osha arrayning eng katta qiymatiga tegishli birinchi indexni qaytarsin.
+MASALAN: getHighestIndex([5, 21, 12, 21, 8]) return qiladi 1 sonini.
 
-@MITASK
 */
 
-function findDoublers(str) {
-    let arr = str.split('').sort();
-    let arr2 = [];
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] == arr[i + 1]) {
-            arr2.push(arr[i]);
-        }
-    
-    }
-    if (arr2.length > 0) {
-        return true;
-        } else {
-            return false;
-    }
+function getHighestIndex(arr){
+
+
+    let max = arr[0];
+    let index = 0;
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] > max){
+            max = arr[i];
+            index = i;
+        
+        } 
+        
+    } 
+    return index;
 }
-console.log(findDoublers("helol"));
 
+console.log(getHighestIndex([1, 34, 56, 89]))
 
+// the second method
+// function getHighestIndex(arr) {
 
-// function findDoublers(str) {
-//     const sortedStr = str.split('').sort().join('');
+//     const result = arr.reduce((maxIndex, currentValue, currentIndex) => {
+//         return currentValue > arr[maxIndex] ? currentIndex : maxIndex;
+//     }, 0);
 
-//     for (let i = 0; i < sortedStr.length - 1; i++) {
-//         if (sortedStr[i] === sortedStr[i + 1]) {
-//             return true;
-//         }
-//     }
-
-//     return false;
+//     return result
 // }
+// console.log(getHighestIndex([1, 34, 56, 35]))
 
-// console.log(findDoublers("helol"))
+
+
+
